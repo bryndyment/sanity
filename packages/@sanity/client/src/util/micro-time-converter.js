b4ccc175cbe5c5f55513e050db229a0fc9512aff
Rex Isaac Raphael <rex.raphael@outlook.com>
2020-11-26 14:05:18 +0100

    Reverted local package json versio of client and useless file

--- packages/@sanity/client/src/util/micro-time-converter.js
+++
@@ -1,7 +0,0 @@
-exports.microsecondsToMilliseconds = function(microseconds) {
-  return Math.ceil(microseconds / 1000)
-}
-
-exports.millisecondsToMicroseconds = function(milliseconds) {
-  return 1000 * milliseconds
-}
