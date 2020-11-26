62dec98a18964e20c66a3521ea3a462cb75a5f94
Rex Isaac Raphael <rex.raphael@outlook.com>
2020-11-26 12:05:26 +0100

    Removed dataset-alias folder

---
+++ packages/@sanity/core/src/commands/dataset/alias/listAliasesHandler.js
@@ -0,0 +1,44 @@
+import {sortBy, size} from 'lodash'
+import * as aliasClient from './datasetAliasesClient'
+
+const sortFields = ['alias', 'dataset']
+
+export default async (args, context) => {
+  const {apiClient, output, chalk} = context
+  const client = apiClient()
+  const {sort, order} = {
+    sort: 'alias',
+    order: 'asc',
+    ...args.extOptions
+  }
+
+  if (!sortFields.includes(sort)) {
+    throw new Error(`Can't sort by field "${sort}". Must be one of ${sortFields.join(', ')}`)
+  }
+
+  const aliases = await aliasClient.listAliases(client)
+
+  const ordered = sortBy(
+    aliases.map(({name, datasetName}) => {
+      const dn = datasetName ? datasetName : '<unlinked>'
+      return [name, dn]
+    }),
+    [sortFields.indexOf(sort)]
+  )
+
+  const rows = order === 'asc' ? ordered : ordered.reverse()
+
+  const maxWidths = rows.reduce(
+    (max, row) => row.map((current, index) => Math.max(size(current), max[index])),
+    sortFields.map(str => size(str))
+  )
+
+  const printRow = row => {
+    const isUnlinked = row[1] === '<unlinked>'
+    const textRow = row.map((col, i) => `${col}`.padEnd(maxWidths[i])).join('   ')
+    return isUnlinked ? chalk.dim(textRow) : textRow
+  }
+
+  output.print(chalk.cyan(printRow(sortFields)))
+  rows.forEach(row => output.print(printRow(row)))
+}
